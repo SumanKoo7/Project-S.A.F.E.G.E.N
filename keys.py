733b8fcbe561b3dc3ae6cd4baf70b92f788b7262
SUMANKANTH K <121917853+SumanKoo7@users.noreply.github.com>
2024-07-14 08:33:51 +0530

    Credentials of SAFEGEN

---
+++ keys.py
@@ -0,0 +1,13 @@
+# Twilio 
+account_sid = 'Your Twilio Account Sid'
+auth_token = 'Your Twilio Authentication Token'
+twilio_no = 'Your_Twilio Phone Number'
+reciever_no = 'Your Reciever's Number' # make sure with country code
+
+# SMTP 
+Sender_Email ='safegenalerts@gmail.com'
+Receiver_Email = 'Your Reciever's Mail ID'
+Subject = 'Alert From S.A.F.E.G.E.N'
+Message = 'Fire Detected!'
+
+server = smtplib.SMTP("smtp.gmail.com", 587)
